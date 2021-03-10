6b66f8bec4f98644a2b2f445e38bde1fc98a4c1c
frank-roesler <65922825+frank-roesler@users.noreply.github.com>
2021-03-10 17:14:58 +0000

    Update TriSpec.m

--- TriSpec.m
+++ TriSpec.m
@@ -3,9 +3,9 @@
 % representing one period for each diagonal.
 
 clear;
-M        = 100; 
-theta    = linspace(0,2*pi,M);
-L        = build_lattice(-2-3i, 4+3i, M);
+M        = 40; 
+theta    = linspace(0,2*pi,round(2*pi*M));
+L        = build_lattice(-2-2.5i, 3+2.5i, M);
 npts     = length(L(:));
 Spectrum = zeros(size(L));
 
@@ -16,8 +16,8 @@ Aplus  = [-1; -2; 1; 3i; -5];
 
 N      = length(Adiag); % Period
 
-ctr = M/30;
-disp('______________________________')
+ctr = length(theta)/20;
+disp('____________________')
 for k=1:length(theta)
     Aminus_theta = circshift(Aminus,1)*exp(-1i*theta(k)/N);
     Aplus_theta = circshift(Aplus,-1)*exp(1i*theta(k)/N);
@@ -27,12 +27,12 @@ for k=1:length(theta)
     parfor j=1:npts
         z = L(j);
         v = flip(z.^(0:N));
-        S(j) = abs(v*p) < 10/sqrt(M);
+        S(j) = abs(v*p) < 20/sqrt(M);
     end
     Spectrum = Spectrum | S;
     if k >= ctr
         fprintf('=')
-        ctr = ctr+M/30;
+        ctr = ctr+length(theta)/20;
     end
 end
 disp(' ')
